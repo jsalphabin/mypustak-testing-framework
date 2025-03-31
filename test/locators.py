70e30a2ae19a618c414357c3f9e85d1ec3512e9e
Jainam Shah <jainam.alphabin@gmail.com>
2025-03-31 17:36:00 +0530

    gitignore added, conftest added, path changed from absolute to relative

--- test/locators.py
+++ test/locators.py
@@ -19,16 +19,16 @@ class LocateByClass:
 class LocateByXpath:
   SEARCH_BAR_XPATH = '//input[@id="gsearch"]'  # Adjust this if needed
   SEARCH_BUTTON_XPATH = '//button[@id="searchId"]'  # Adjust this if needed
-  RESULT_TITLES_XPATH = '//*[contains(@class, "search-page")]/div/div/div/div/div/div[1]'  # Modify based on actual site structure
+  RESULT_TITLES_XPATH = '(//*[contains(@class, "search-page")]//div[contains(@class,"BookCard")])[1]'  # Modify based on actual site structure
   ERROR_MESSAGE_XPATH='//*[contains(@class, "title-error")]'
-  EMAIL_INPUT_XPATH = '//div[@class="WLoginNavbar_loginDialogRightDiv__x6Kbk"]/form/div/div[1]/div/div/input'
-  PASSWORD_INPUT_XPATH = '//div[@class="WLoginNavbar_loginDialogRightDiv__x6Kbk"]/form/div/div[2]/div/input'
-  EMAIL_ERROR_XPATH = '//div[@class="WLoginNavbar_loginDialogRightDiv__x6Kbk"]/form/div/div[1]/div/p'
+  EMAIL_INPUT_XPATH = '(//div[@class="WLoginNavbar_loginDialogRightDiv__x6Kbk"]//input)[1]'
+  PASSWORD_INPUT_XPATH = '(//div[@class="WLoginNavbar_loginDialogRightDiv__x6Kbk"]//input)[2]'
+  EMAIL_ERROR_XPATH = '(//div[@class="WLoginNavbar_loginDialogRightDiv__x6Kbk"]//p)[1]'
   ADD_TO_CART_XPATH="//div[contains(@class,'search-page')]//button[contains(., 'Add to Cart')][1]"
   ADD_TO_CART_FINAL_XPATH = "(//button[contains(@class, 'Product_productAddtoCarddiv__hWTQk')])[1]"
   BOOK_ADDED_TO_CART_XPATH="//span[contains(text(),'Book added to cart!')]"
   CART_ICON="//span[@id='cartIcon']"
   LOGIN_BTN='//button[@id="loginBtn"]/span'
-  ADD_BUTTON='//*[@id="yousaved"]/div/div/button[2]'
-  QUANTITY='//*[@id="yousaved"]/div/div/span[1]'
-  ZERO_XPATH='/html/body/main/div[3]/div/div/div/div[1]/div[1]/div[1]/div[1]/span/text()[2]'
+  ADD_BUTTON='(//*[@id="yousaved"]//button)[2]'
+  QUANTITY='(//*[@id="yousaved"]//button)[2]/preceding-sibling::span'
+  ZERO_XPATH='(//div[contains(@class,"CartPage_cartLeft__hoGvn")]//span)[1]'
